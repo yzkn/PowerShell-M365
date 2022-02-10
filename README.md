1c93d3a2c8e28b44933377eafb35c38c582763a9
YA <YA-androidapp@users.noreply.github.com>
2022-02-10 12:53:38 +0900

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# PowerShell-M365
+
+---
+
+* [PowerShell を使用して Microsoft 365 に接続する](https://docs.microsoft.com/ja-jp/microsoft-365/enterprise/connect-to-microsoft-365-powershell)
+* [単一の PowerShell ウィンドウですべての Microsoft 365 サービスに接続する](https://docs.microsoft.com/ja-jp/microsoft-365/enterprise/connect-to-all-microsoft-365-services-in-a-single-windows-powershell-window)
+  * Azure Active Directory V2
+  * SharePoint Online Management Shell
+  * Exchange Online PowerShell V2
+  * Teams PowerShell
+
+---
+
+Copyright (c) 2022 YA-androidapp(https://github.com/YA-androidapp) All rights reserved.
---
+++ o365.ps1
@@ -0,0 +1,63 @@
+﻿# Copyright (c) 2022 YA-androidapp(https://github.com/YA-androidapp) All rights reserved.
+
+
+function Install-AzureADModule {
+    Install-Module -Name AzureAD -Force -AllowClobber
+    Write-Host "Azure AD module has been installed"
+}
+
+function Install-SPOModule {
+    Install-Module -Name Microsoft.Online.SharePoint.PowerShell -Force -AllowClobber
+    Write-Host "SPO module has been installed"
+}
+
+function Install-TeamsModule {
+    Install-Module -Name MicrosoftTeams -Force -AllowClobber
+    Write-Host "Teams module has been installed"
+}
+
+function Install-ExchangeOnline {
+    Install-Module -Name ExchangeOnlineManagement -Force -AllowClobber
+    Write-Host "EXO module has been installed"
+}
+
+function Pause {
+    if ($psISE) {
+        $null = Read-Host 'Press Enter Key...'
+    }
+    else {
+        Write-Host "Press Any Key..."
+        (Get-Host).UI.RawUI.ReadKey("NoEcho,IncludeKeyDown") | Out-Null
+    }
+}
+
+
+function Install-Modules {
+    if ($PSVersionTable["PSVersion"].Major -lt 7) {
+        Invoke-Expression "& { $(Invoke-RestMethod https://aka.ms/install-powershell.ps1) } -UseMSI"
+        exit
+    }
+    else {
+        $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+        $isAdmin = $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
+        if (!$isAdmin) {
+            Start-Process pwsh "-File `"$PSCommandPath`"" -Verb RunAs
+            exit
+        }
+        else {
+            Set-ExecutionPolicy RemoteSigned
+            Write-Host "Set-ExecutionPolicy has been set"
+
+            Install-AzureADModule
+            Install-SPOModule
+            Install-TeamsModule
+            Install-ExchangeOnline
+
+            Pause
+        }
+    }
+}
+
+
+Install-Modules
