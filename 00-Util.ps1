7603cbe46b6356acec82f41d621efe7c219db37f
YA <YA-androidapp@users.noreply.github.com>
2023-04-26 07:39:24 +0900

    Updates

--- 00-Util.ps1
+++ 00-Util.ps1
@@ -1,3 +1,6 @@
+# Copyright (c) 2023 YA-androidapp(https://github.com/YA-androidapp) All rights reserved.
+
+
 function Get-Config {
     $Path = "settings.ini"
 
--- 02-認証.ps1
+++ 02-認証.ps1
@@ -1,3 +1,6 @@
+# Copyright (c) 2023 YA-androidapp(https://github.com/YA-androidapp) All rights reserved.
+
+
 .".\00-Util.ps1"
 
 
